e5016be537135e28b4841a169717d38483694b7c
henrique-lh <henrique.santos.lhls@gmail.com>
2024-01-16 16:19:32 -0300

    add cars-assemble solution

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,20 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / float64(100)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	var group_of_cars int = carsCount / 10 
+  individual_cars := carsCount % 10 
+  return uint(group_of_cars * 95000) + uint(individual_cars * 10000)
+}
