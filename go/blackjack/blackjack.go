0336baad6eb4aaf770b7ae6051efefe7935fc3d7
henrique-lh <henrique.santos.lhls@gmail.com>
2024-01-22 10:44:53 -0300

    add blackjack solution

---
+++ go/blackjack/blackjack.go
@@ -0,0 +1,54 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch {
+    case card == "ace": 
+      return 11
+    case card == "one": 
+      return 1 
+    case card == "two": 
+      return 2 
+    case card == "three": 
+      return 3
+    case card == "four":
+      return 4
+    case card == "five":
+      return 5
+    case card == "six":
+      return 6
+    case card == "seven":
+      return 7
+    case card == "eight":
+      return 8
+    case card == "nine":
+      return 9
+    case card == "ten":
+      return 10
+    case card == "jack" || card == "queen" || card == "king":
+      return 10
+    default:
+      return 0
+  }
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+	switch {
+    case card1 == "ace" && card2 == "ace":
+      return "P"
+    case ParseCard(card1) + ParseCard(card2) == 21 && ParseCard(dealerCard) < 10:
+      return "W"
+    case ParseCard(card1) + ParseCard(card2) == 21 && ParseCard(dealerCard) >= 10:
+      return "S"
+    case ParseCard(card1) + ParseCard(card2) >= 17 && ParseCard(card1) + ParseCard(card2) <= 20:
+      return "S"
+    case ParseCard(card1) + ParseCard(card2) >= 12 && ParseCard(card1) + ParseCard(card2) <= 16 && ParseCard(dealerCard) >= 7:
+      return "H"
+     case ParseCard(card1) + ParseCard(card2) >= 12 && ParseCard(card1) + ParseCard(card2) <= 16 && ParseCard(dealerCard) < 7:
+      return "S"
+    default:
+      return "H"
+  }
+}
