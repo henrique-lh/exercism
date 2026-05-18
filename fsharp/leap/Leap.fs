36f2b99bff195411e434c2ffc4fa40434f07c739
Luis Henrique <75594301+henrique-lh@users.noreply.github.com>
2026-05-18 15:11:55 -0300

    Delete fsharp directory

--- fsharp/leap/Leap.fs
+++
@@ -1,9 +0,0 @@
-module Leap
-
-let leapYear (year: int): bool = 
-    if year % 4 = 0 && year % 100 <> 0 then 
-        true
-    elif year % 100 = 0 && year % 400 = 0 then 
-        true
-    else
-        false
